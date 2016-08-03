baceafa897c378fc11f10fb977562fbe11c3d1ed
asupranovich <meatbeaton@tut.by>
2016-08-03 18:04:55 +0300

    lecture 20

---
+++ start/lecture20/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+   var info;
+   
+   if(req.url === '/') {
+       info = fs.readFile('index.html', function(err, info){
+           if(err) {
+               console.error(err);
+               res.statusCode = 500;
+               res.end("Error occured on server!");
+               return;
+           }
+           res.end(info);
+       });
+   } else if (req.url === '/now') {
+       res.end(new Date().toString());
+   }
+}).listen(3000);
